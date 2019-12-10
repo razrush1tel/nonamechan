37168a62d79b9da7beec4bcb0587e69a9d0b35c0
Anton Modlyageev <celka228@gmail.com>
2019-12-10 16:19:11 +0300

    no db script now

--- create_db.py
+++
@@ -1,7 +0,0 @@
-from package import create_app, db
-
-if __name__ == '__main__':
-    app = create_app()
-    ctx = app.app_context()
-    ctx.push()
-    db.create_all()
--- run.py
+++ run.py
@@ -4,5 +4,8 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
+    ctx = app.app_context()
+    ctx.push()
+    db.create_all()
     port = int(os.environ.get("PORT", 5000))
     app.run(port=port, host='0.0.0.0', debug=True)
