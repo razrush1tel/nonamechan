972247907e88c87098f5e17e57460952f51bc102
rischev <romanscherbakov.i@gmail.com>
2019-12-17 13:27:56 +0300

    fixed crash when searching nonexistent tag

--- package/main/routes.py
+++ package/main/routes.py
@@ -16,6 +16,7 @@ def home():
 
 @main.route('/search', methods=['GET', 'POST'])
 def search():
+    empty = True
     try:
         tags = request.form['query'].split(', ')
     except KeyError:
@@ -24,14 +25,17 @@ def search():
     page = request.args.get('page', 1, type=int)
     sets = []
     for tag in tags:
-        post_ids = Tag.query.filter_by(name=tag).first().post_list
-        id_set = set()
-        for post_id in post_ids:
-            id_set.add(post_id.post_id)
-        sets.append(id_set)
-    while len(sets) > 1:
-        sets[0] = sets[0].intersection(sets[1])
-        sets.pop(1)
-    sets = sets[0]
+        post_ids = Tag.query.filter_by(name=tag).first()
+        if post_ids is not None:
+            post_ids = post_ids.post_list
+            id_set = set()
+            for post_id in post_ids:
+                id_set.add(post_id.post_id)
+            sets.append(id_set)
+            while len(sets) > 1:
+                sets[0] = sets[0].intersection(sets[1])
+                sets.pop(1)
+            sets = sets[0]
+            empty = False
     posts = Post.query.filter(Post.id.in_(sets)).order_by(Post.date_posted.desc()).paginate(per_page=24, page=page)
-    return render_template('search.html', posts=posts, searchform=searchform, filter=', '.join(tags))
+    return render_template('search.html', posts=posts, emptry=empty, searchform=searchform, filter=', '.join(tags))
